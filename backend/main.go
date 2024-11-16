a4a4e72ede42dd6163ca96f2275fcdca7e9a0ebd
Saromazzotta <saromazzotta@gmail.com>
2024-11-16 14:30:15 -0500

    added in login route and function using csrftokens and session tokens

--- backend/main.go
+++ backend/main.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"time"
 
 	_ "github.com/lib/pq"
 	"github.com/saromazzotta/go_finance/middleware"
@@ -20,9 +21,10 @@ type User struct {
 }
 
 type Login struct {
-	HashedPassword string
-	SessionToken   string
-	CSRFToken      string
+	Username     string
+	Password     string
+	SessionToken string
+	CSRFToken    string
 }
 
 // Global database connection
@@ -66,6 +68,7 @@ func register(w http.ResponseWriter, r *http.Request) {
 
 	if len(user.Password) < 6 {
 		http.Error(w, "Password must be at least 6 characters long", http.StatusBadRequest)
+		return
 	}
 
 	if user.Password != user.ConfirmPassword {
@@ -96,6 +99,94 @@ func register(w http.ResponseWriter, r *http.Request) {
 
 }
 
+// --------------------- LOGIN ----------------------------------------------------------
+func login(w http.ResponseWriter, r *http.Request) {
+	// Method Check
+	if r.Method != http.MethodPost {
+		http.Error(w, "Invalid method", http.StatusMethodNotAllowed)
+		return
+	}
+
+	var login Login
+
+	// Decode json body
+	err := json.NewDecoder(r.Body).Decode(&login)
+	if err != nil {
+		http.Error(w, "Invalid JSON payload", http.StatusBadRequest) // gives a 400 error
+		return
+	}
+
+	// Validations
+	if login.Username == "" || login.Password == "" {
+		http.Error(w, "Username and password cannot be blank", http.StatusBadRequest)
+		return
+	}
+
+	// Fetch user credentials from database
+	var storedPassword string
+	err = db.QueryRow("SELECT password FROM users WHERE username=$1", login.Username).Scan(&storedPassword)
+	if err == sql.ErrNoRows {
+		http.Error(w, "Invalid username or password", http.StatusUnauthorized)
+		return
+	} else if err != nil {
+		log.Println("Database error: ", err)
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError) // returns a 500 error
+		return
+	}
+	log.Printf("Fetched password from database: %s\n", storedPassword)
+
+	// authenticate user
+
+	if !middleware.CheckPasswordHash(login.Password, storedPassword) {
+		http.Error(w, "Invalid username or password", http.StatusUnauthorized)
+		return
+	}
+
+	// genereate tokens with middleware
+	login.SessionToken = middleware.GenerateToken(32)
+	login.CSRFToken = middleware.GenerateToken(32)
+
+	// set session cookie
+	http.SetCookie(w, &http.Cookie{
+		Name:     "session_token",
+		Value:    login.SessionToken,
+		Expires:  time.Now().Add(24 * time.Hour),
+		HttpOnly: true,
+	})
+
+	// set CSRFToken cookie
+	http.SetCookie(w, &http.Cookie{
+		Name:     "csrf_token",
+		Value:    login.CSRFToken,
+		Expires:  time.Now().Add(24 * time.Hour),
+		HttpOnly: false, // needs to be accessible to the client-side
+	})
+
+	// store tokens in the database
+	stmt := `
+		INSERT INTO user_sessions (username, session_token, csrf_token, updated_at)
+		VALUES ($1, $2, $3, CURRENT_TIMESTAMP)
+		ON CONFLICT (username) DO UPDATE
+		SET session_token = EXCLUDED.session_token,
+			csrf_token = EXCLUDED.csrf_token,
+			updated_at = CURRENT_TIMESTAMP
+	`
+
+	log.Printf("Inserting session token for user: %s\n", login.Username)
+	_, err = db.Exec(stmt, login.Username, login.SessionToken, login.CSRFToken)
+	if err != nil {
+		log.Println("Failed to save session token: ", err)
+		http.Error(w, "Failed to log in ", http.StatusInternalServerError)
+		return
+	}
+
+	// Log the successful login attempt
+	log.Printf("Login Attempt: %+v\n", login)
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(map[string]string{"message": "Login successful!"})
+}
+
 // func testHandler(w http.ResponseWriter, r *http.Request) {
 // 	response := map[string]string{
 // 		"message": "Hi from the backend!",
@@ -122,6 +213,7 @@ func main() {
 	// -------------------- Routes-----------------------------
 	// router.HandleFunc("/hello", testHandler)
 	router.HandleFunc("/api/users/register", register)
+	router.HandleFunc("/api/users/login", login)
 
 	server := http.Server{
 		Addr:    ":8080",
